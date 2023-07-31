41465ffe631aecffda5530f9a5c6382280ad3a45
Yegor <egorkrylatov18@gmail.com>
2023-07-31 15:02:17 +0500

    class for String data

--- app/src/main/java/hexlet/code/StringSchema.java
+++ app/src/main/java/hexlet/code/StringSchema.java
@@ -1,2 +1,37 @@
-package hexlet.code;public class StringSchema {
+package hexlet.code;
+
+import hexlet.code.constraints.Constraint;
+import hexlet.code.constraints.IsString;
+import hexlet.code.constraints.MinLength;
+import hexlet.code.constraints.Required;
+import hexlet.code.constraints.Contains;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringSchema {
+    List<Constraint> constraints = new ArrayList<>();
+    StringSchema() {
+        constraints.add(new IsString());
+    }
+    public StringSchema required() {
+        constraints.add(new Required());
+        return this;
+    }
+    public StringSchema minLength(int minLength) {
+        constraints.add(new MinLength(minLength));
+        return this;
+    }
+    public StringSchema contains(String str) {
+        constraints.add(new Contains(str));
+        return this;
+    }
+    public boolean isValid(Object value) {
+        for (Constraint constraint: constraints) {
+            if (!constraint.validate(value)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
