eb8a6e777251cf7372cc829f4c24c9dcbdfd8c29
Yegor <egorkrylatov18@gmail.com>
2023-08-01 10:21:15 +0500

    add test cases for a Map schema

--- app/src/test/java/hexlet/code/MapSchemaTest.java
+++ app/src/test/java/hexlet/code/MapSchemaTest.java
@@ -1,2 +1,30 @@
-package hexlet.code;public class MapSchemaTest {
+package hexlet.code;
+
+import hexlet.code.schemas.MapSchema;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MapSchemaTest {
+    @Test
+    public void isValidTest() {
+        Validator v = new Validator();
+        MapSchema schema = v.map();
+        assertTrue(schema.isValid(null));
+        schema.required();
+        assertFalse(schema.isValid(null));
+        assertTrue(schema.isValid(new HashMap()));
+        Map<String, String> data = new HashMap<>();
+        data.put("key1", "value1");
+        assertTrue(schema.isValid(data));
+        schema.sizeof(2);
+        assertFalse(schema.isValid(data));
+        data.put("key2", "value2");
+        assertTrue(schema.isValid(data));
+
+    }
 }
