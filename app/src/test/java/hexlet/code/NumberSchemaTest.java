f339b526d1b9fb40bd7f158693a5919c83a133d2
Yegor <egorkrylatov18@gmail.com>
2023-08-01 09:36:28 +0500

    add test cases for a NumberSchema

--- app/src/test/java/hexlet/code/NumberSchemaTest.java
+++ app/src/test/java/hexlet/code/NumberSchemaTest.java
@@ -1,2 +1,28 @@
-package hexlet.code;public class NumberSchemaTest {
+package hexlet.code;
+
+import hexlet.code.schemas.NumberSchema;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class NumberSchemaTest {
+    @Test
+    public void isValidTest() {
+        Validator v = new Validator();
+        NumberSchema schema = v.number();
+        assertTrue(schema.isValid(null));
+        assertTrue(schema.positive().isValid(null));
+        schema.required();
+        assertFalse(schema.isValid(null));
+        assertFalse(schema.isValid("5"));
+        assertTrue(schema.isValid(10));
+        assertFalse(schema.isValid(-10));
+        assertFalse(schema.isValid(0));
+        schema.range(5, 10);
+        assertTrue(schema.isValid(5));
+        assertTrue(schema.isValid(10));
+        assertFalse(schema.isValid(4));
+        assertFalse(schema.isValid(11));
+    }
 }
