5415138b54a4fe025a7094f0d78ed47f96614bb1
Yegor <egorkrylatov18@gmail.com>
2023-08-01 09:36:55 +0500

    add a Number Schema

--- app/src/main/java/hexlet/code/schemas/NumberSchema.java
+++ app/src/main/java/hexlet/code/schemas/NumberSchema.java
@@ -1,2 +1,24 @@
-package hexlet.code.schemas;public class NumberSchema {
+package hexlet.code.schemas;
+
+import hexlet.code.constraints.numberschema.IsNumber;
+import hexlet.code.constraints.numberschema.Positive;
+import hexlet.code.constraints.Required;
+import hexlet.code.constraints.numberschema.Range;
+
+public class NumberSchema extends BaseSchema {
+    public NumberSchema() {
+        constraints.add(new IsNumber());
+    }
+    public NumberSchema required() {
+        constraints.add(new Required());
+        return this;
+    }
+    public NumberSchema positive() {
+        constraints.add(new Positive());
+        return this;
+    }
+    public NumberSchema range(int lowBound, int topBound) {
+        constraints.add(new Range(lowBound, topBound));
+        return this;
+    }
 }
