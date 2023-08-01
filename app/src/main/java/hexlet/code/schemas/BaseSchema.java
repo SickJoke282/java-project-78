9e6f4bde10777ba3746cd9f45b7249d55f7a9bce
Yegor <egorkrylatov18@gmail.com>
2023-08-01 09:37:52 +0500

    define the repeating method isValid

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -1,2 +1,18 @@
-package hexlet.code.schemas;public class BaseSchema {
+package hexlet.code.schemas;
+
+import hexlet.code.constraints.Constraint;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BaseSchema {
+    List<Constraint> constraints = new ArrayList<>();
+    public boolean isValid(Object value) {
+        for (Constraint constraint: constraints) {
+            if (!constraint.validate(value)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
