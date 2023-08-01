dcb08637ed7accab79fc69266b564e6187f3fc60
Yegor <egorkrylatov18@gmail.com>
2023-08-01 11:12:37 +0500

    add a shape constraint

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -3,6 +3,9 @@ package hexlet.code.schemas;
 import hexlet.code.constraints.Required;
 import hexlet.code.constraints.mapschema.IsMap;
 import hexlet.code.constraints.mapschema.Sizeof;
+import hexlet.code.constraints.mapschema.Shape;
+
+import java.util.Map;
 
 public class MapSchema extends BaseSchema {
     public MapSchema() {
@@ -14,6 +17,10 @@ public class MapSchema extends BaseSchema {
     }
     public MapSchema sizeof(int limit) {
         constraints.add(new Sizeof(limit));
+        return this;
+    }
+    public MapSchema shape(Map<String, BaseSchema> schemas) {
+        constraints.add(new Shape(schemas));
         return this;
     }
 }
