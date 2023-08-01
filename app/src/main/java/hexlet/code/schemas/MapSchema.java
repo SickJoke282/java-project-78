4038ce2cacbae25618bc172e1ddbed7d0f6c9a60
Yegor <egorkrylatov18@gmail.com>
2023-08-01 10:22:35 +0500

    add a new schema which validate HashMaps

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -1,2 +1,19 @@
-package hexlet.code.schemas;public class MapSchema {
+package hexlet.code.schemas;
+
+import hexlet.code.constraints.Required;
+import hexlet.code.constraints.mapschema.IsMap;
+import hexlet.code.constraints.mapschema.Sizeof;
+
+public class MapSchema extends BaseSchema {
+    public MapSchema() {
+        constraints.add(new IsMap());
+    }
+    public MapSchema required() {
+        constraints.add(new Required());
+        return this;
+    }
+    public MapSchema sizeof(int limit) {
+        constraints.add(new Sizeof(limit));
+        return this;
+    }
 }
