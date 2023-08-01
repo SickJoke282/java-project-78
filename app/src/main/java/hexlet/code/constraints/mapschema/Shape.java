0416eb9bc413c4c3193dacc819604e24864af6bb
Yegor <egorkrylatov18@gmail.com>
2023-08-01 11:12:20 +0500

    constraint for inner data

---
+++ app/src/main/java/hexlet/code/constraints/mapschema/Shape.java
@@ -0,0 +1,23 @@
+package hexlet.code.constraints.mapschema;
+
+import hexlet.code.constraints.Constraint;
+import hexlet.code.schemas.BaseSchema;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Shape implements Constraint {
+    private final Map<String, BaseSchema> schemas;
+    public Shape(Map<String, BaseSchema> schemas) {
+        this.schemas = schemas;
+    }
+    @Override
+    public boolean validate(Object value) {
+        for (Map.Entry<String, Object> entry: ((HashMap<String, Object>) value).entrySet()) {
+            if (!schemas.get(entry.getKey()).isValid(entry.getValue())) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
